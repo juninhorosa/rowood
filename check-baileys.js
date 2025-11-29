08e77523d3b6bf96dac10fb42d43c7180f2b8c2e
juninhorosa <42284444+juninhorosa@users.noreply.github.com>
2025-11-29 23:58:13 +0000

    Switch to require for importing package.json

--- check-baileys.js
+++ check-baileys.js
@@ -1,3 +1,6 @@
-import pkg from '@whiskeysockets/baileys/package.json' assert { type: "json" };
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const pkg = require("@whiskeysockets/baileys/package.json");
 
 console.log("📦 Baileys instalado no Render:", pkg.version);
